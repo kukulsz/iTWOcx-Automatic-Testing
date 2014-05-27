4267ab41a141006fa2473617bdd6e347ec9ea6e6
John.Li <lsz117@163.com>
2014-05-27 17:25:14 +0800

    change IEDriverServer path make URL parameterized

--- iTWOcx/src/Utilities/Config.java
+++ iTWOcx/src/Utilities/Config.java
@@ -9,22 +9,24 @@ import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 public class Config {
 	public static WebDriver driver=null;
 	public static Selenium selenium=null;
-	public static final String URL_18A="http://gzweb01.projectcentre.loc/cx18a/cx.aspx?j=LERYTESTPROJECT";
-	public static final String UPLOAD_FILE_LOCATION="D:\\JAVA Study\\iTWOcx\\Test Data\\DOC1.jpg";
+	public static final String version = "cx18cpi";
+	public static final String projectName = "pro18b9";
+	public static String URL="http://gzweb01.projectcentre.loc/" + version + "/cx.aspx?j=" + projectName;
+	public static final String UPLOAD_FILE_LOCATION="Test Data\\DOC1.jpg";
 
 	
 
 	public static void IEWebDriverStart(){
 		// start a driver
-			System.setProperty ("webdriver.ie.driver", "D:/Tools/Selenium/IEDriverServer_x64_2.41.0/IEDriverServer.exe");
+			System.setProperty ("webdriver.ie.driver", "Res/IEDriverServer.exe");
 			driver=new InternetExplorerDriver();
 		// open testing URL
-			driver.get(URL_18A);
+			driver.get(URL);
 		}
 
 	// initial a Selenium driver
 	public static Selenium getSelenium(){
-		return new WebDriverBackedSelenium(driver, URL_18A);
+		return new WebDriverBackedSelenium(driver, URL);
 	}
 	
 	public static void closeBrowser(){
